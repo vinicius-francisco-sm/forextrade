df7e7a57323dc28297877029e16f8b2ba69bcafd
vinicius-francisco-sm <viniciussiqueira166@gmail.com>
2026-04-24 22:01:55 -0300

    feat: implement gsap scroll smoother

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,33 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 
+// smooth scroll
+import gsap from "gsap";
+import { ScrollTrigger, ScrollSmoother } from "gsap/all";
+import { useRef, useEffect } from "react";
+
 const App = () => {
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+    const smoother = ScrollSmoother.create({
+      content: "#smooth-content",
+      smooth: 1.8,
+      effects: true,
+    });
+
+    return () => {
+      smoother && smoother.kill();
+      ScrollTrigger.getAll().forEach((t) => t.kill());
+    };
+  }, []);
+
   return (
     <div className="text-zinc-100 bg-black p-4">
       <NavBar />
 
-      <div className="">
+      <div id="smooth-content" ref={contentRef}>
         <HomePage />
       </div>
     </div>
