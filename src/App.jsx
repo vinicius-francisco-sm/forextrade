3ac6125b787decfb4b16f8d174d1299834d7333b
vinicius-francisco-sm <viniciussiqueira166@gmail.com>
2026-04-24 21:49:11 -0300

    refactor: rearranging the code structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
-import LogoCarousel from "./sections/LogoCarousel";
-import FeaturesSection from "./sections/FeaturesSection";
-import PricingSection from "./sections/PricingSection";
-import TestimonialSection from "./sections/TestimonialSection";
-import CtaSection from "./sections/CtaSection";
-import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className="text-zinc-100 bg-black p-4">
       <NavBar />
 
-      <HomePage />
-      <LogoCarousel />
-      <FeaturesSection />
-      <PricingSection />
-      <TestimonialSection />
-      <CtaSection />
-      <Footer />
+      <div className="">
+        <HomePage />
+      </div>
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import HeroSection from "../sections/HeroSection";
+import LogoCarousel from "../sections/LogoCarousel";
+import FeaturesSection from "../sections/FeaturesSection";
+import PricingSection from "../sections/PricingSection";
+import TestimonialSection from "../sections/TestimonialSection";
+import CtaSection from "../sections/CtaSection";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
   return (
     <div className="bg-black mx-auto">
       <HeroSection />
+      <LogoCarousel />
+      <FeaturesSection />
+      <PricingSection />
+      <TestimonialSection />
+      <CtaSection />
+      <Footer />
     </div>
   );
 };
